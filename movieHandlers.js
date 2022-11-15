7491a77e3febec70a51925681fe90d5d08096169
OUASMI <souad@MBP-de-OUASMI.lan>
2022-11-15 09:46:51 +0100

    express 4

--- movieHandlers.js
+++ movieHandlers.js
@@ -116,6 +116,42 @@ const postUser = (req, res) => {
       res.status(500).send("Error saving the user");
     });
 };
+const updateMovie = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { title, director, year, color, duration } = req.body;
+
+  database
+    .query("UPDATE movies SET title = ?, director = ?,year = ?, color = ?, duration = ? where id = ?", [title, director, year, color, duration, id])
+    .then(([result]) => {
+      if (result.affectedRows===0 ) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error update movie");
+    });
+};
+const updateUser = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { firstname, lastname, email, city, language } = req.body;
+
+  database
+    .query("UPDATE users SET firstname = ?, lastname = ?,email = ?, city = ?, language = ? where id = ?", [firstname, lastname, email, city, language , id])
+    .then(([result]) => {
+      if (result.affectedRows===0 ) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error update movie");
+    });
+};
 
 
 module.exports = {
@@ -125,4 +161,6 @@ module.exports = {
   getUsersById,
   postMovie,
   postUser,
+  updateMovie,
+  updateUser,
 };
